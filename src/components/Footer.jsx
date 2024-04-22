5f65086e12461089b74414d61bf20d52be00e0e8
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-04-22 16:44:04 -0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,42 +1,31 @@
 import React from "react";
 
-
 const Footer = () => {
   return (
     <div>
       <div className="footer bg-blue-900 text-white">
-        <p className="text-blue-200 text-center pt-20">Start Now</p>
-        <p className="font-bold pt-2 pl-20 text-center ">Sign up to learn more</p>
-        <p className="text-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/> Quibusdam
-          ducimus, nulla at inventore corrupti animi ipsa molestiae sequi
-          laudantium debitis nostrum a cum modi!
+        <p className="font-bold pt-10 pl-20 text-center ">
+          Sign up to learn more
         </p>
-        <input type="text" name="text" placeholder="Enter your email" className=" " />
-        <button className="">Subscribe</button>
-        <div className="footer-section bg-blue-600 ml-20 mr-20 mt-20">
-          <p className="">Coinfied</p>
-          <p className="">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-            omnis commodi, nihil veritatis inventore earum autem suscipit vel
-            obcaecati optio cupiditate fuga natus aperiam.
-          </p>
-          <section className="">
-            <p>Our Products</p>
-            <p>Crypto Earn</p>
-            <p>Exchanges</p>
-            <p>Defi Wallet</p>
-            <p>Converter</p>
-            <p>Pages</p>
-            <p>About Us</p>
-            <p>Our App</p>
-            <p>Careers</p>
-            <p>Token Safe</p>
-            <p>Contact</p>
-            <p>08083729474</p>
-            <p>Mon-Fri: 9am-11pm</p>
-            <p>Coinfied@gmail.com</p>
-          </section>
+        <p className="text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          <br /> Quibusdam ducimus, nulla at inventore corrupti animi ipsa
+          molestiae sequi laudantium debitis nostrum a cum modi!
+        </p>
+        <div className="flex justify-center">
+          <input
+            type="text"
+            name="text"
+            placeholder="Enter your email"
+            className="items-center bg-blue-500 mt-5 mb-5 text-white py-2 px-4 rounded "
+          />
+          <button className="bg-blue-400 hover:bg-blue-700 text-white py-1 px-2">
+            Subscribe
+          </button>
+        </div>
+
+        <div className=" text-center mt-20 bg-blue-800 pt-5 pb-5">
+          <p>©2024 Coinfied. All rights Reserved</p>
         </div>
       </div>
     </div>
