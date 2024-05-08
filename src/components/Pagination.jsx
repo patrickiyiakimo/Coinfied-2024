0868224da8e80c783452dad6b20b1f8b35c50dfa
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-05-08 16:51:46 +0100

    Update Pagination.jsx

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -10,7 +10,7 @@ const Pagination = ({ totalPosts, postsPerPage, setCurrentPage }) => {
   return (
     <>
       {pages.map((page, index) => {
-        return <button key={index} onClick={() => setCurrentPage(page)}>{page}</button>;
+        return  <button key={index} onClick={() => setCurrentPage(page)} className={`bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded ${page === currentPage ? "bg-gray-600" : ""}`}>{page}</button>;
       })}
     </>
   );
