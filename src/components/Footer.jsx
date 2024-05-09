2042235c9ae0944772ded27ed82d8071e90c9e7c
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-05-09 18:17:25 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 const Footer = () => {
   const [email, setEmail] = React.useState('')
-  // const [isDisable, setIsDisable] = React.useState(false)
+  
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -13,6 +13,16 @@ const Footer = () => {
   
   const subscribe = () => {
     if (email.trim() === '') {
+      toast.error("Please enter your email", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      })
       return;
     }
     toast.success("Thanks For Subscribing!", {
@@ -43,13 +53,13 @@ const Footer = () => {
         <div className="justify-center items-center">
           <input
             onChange={handleEmailChange}
-            type="text"
-            name="text"
+            type="email"
+            value={email}
             placeholder="Enter your email"
             className="ml-96 bg-blue-500 mt-5 mb-5 text-pink-400 py-2 px-4  "
             style={{ marginLeft: 500 }}
           />
-          <button onClick={subscribe } className="bg-gradient-to-r from-blue-500 to-gray-500 text-white font-bold py-2 px-4 hover:from-gray-500 hover:to-blue-500" disabled={email.trim() === ''}>
+          <button onClick={subscribe } className="bg-gradient-to-r from-blue-500 to-gray-500 text-white font-bold py-2 px-4 hover:from-gray-500 hover:to-blue-500">
             Subscribe
           </button>
         </div>
