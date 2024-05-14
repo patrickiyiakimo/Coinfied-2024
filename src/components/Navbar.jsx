b59d2aa05b75a8334466254f9cc473d666e76b39
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-05-14 12:29:52 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -102,9 +102,11 @@ import { FaTimes } from "react-icons/fa";
 const Navbar = () => {
   const [isSetMenu, setIsSetMenu] = React.useState(false);
 
+  
+
   return (
-    <div className="sticky top-0 z-10">
-      <div className="bg-gray-800 text-white text-sm py-1 font-Gill fixed w-full">
+    <div className="sticky top-0 z-10 w-full">
+      <div className="bg-gray-900 shadow-lg text-white text-sm py-1 font-Gill fixed w-full">
         <ul className="flex-col flex space-y-4 md:space-y-0 space-x-4 mb-10 md:mb-0 ">
           {/* Menu button for mobile */}
           <button
@@ -112,18 +114,18 @@ const Navbar = () => {
             onClick={() => setIsSetMenu(!isSetMenu)}
           >
             {isSetMenu ? (
-              <FaTimes className="text-3xl ml-80  mt-6 ml-5 absolute" />
+              <FaTimes className="text-3xl  ml-80 absolute mt-6 ml-5 " />
             ) : (
-              <FiMenu className="mt-6 text-3xl  ml-5 ml-80 absolute" />
+              <FiMenu className="mt-6 text-3xl ml-80 absolute ml-5  " />
             )}
           </button>
           {/* Logo */}
           <li>
-            <h1 className="text-4xl font-bold ">Coinfied</h1>
+            <h1 className="text-4xl font-bold md:flex">Coinfied</h1>
           </li>
           {/* Navbar items */}
           <div
-            className={`md:flex md:flex-row space-x-4 ${
+            className={`md:flex md:justify-end space-x-4 ${
               isSetMenu ? "block" : "hidden"
             }`}
           >
