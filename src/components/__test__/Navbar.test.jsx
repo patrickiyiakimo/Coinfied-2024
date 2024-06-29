e81f06def40e9cea49d05decaf9a1d4f837271df
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-06-29 18:00:02 +0100

    ensured the unit test for the linkitems in the navbar components are passed correctly

--- src/components/__test__/Navbar.test.jsx
+++ src/components/__test__/Navbar.test.jsx
@@ -11,3 +11,13 @@ it("should render the navbar title", async () => {
   const navbarElement = screen.getByTitle("header");
   expect(navbarElement).toBeInTheDocument();
 });
+
+it("should render all the navbar titles in the navbar component", async () => {
+  render(
+    <MemoryRouter>
+      <Navbar />
+    </MemoryRouter>
+  );
+  const navbarElements = screen.getAllByRole("listitem");
+  expect(navbarElements.length).toBe(8);
+});
