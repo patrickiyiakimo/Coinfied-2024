0d1c198d176a6aefbd0231c30dd3187076d13b9b
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-07-01 17:21:34 +0100

    tested for the market components

--- src/components/__test__/Market.test.jsx
+++ src/components/__test__/Market.test.jsx
@@ -1,11 +1,84 @@
+
+// import { render, screen, waitFor } from "@testing-library/react";
+// import { MemoryRouter } from "react-router-dom";
+// import Market from "../Market";
+
+// it("should render the market title", async () => {
+//   render(
+//     <MemoryRouter>
+//       <Market />
+//     </MemoryRouter>
+//   );
+
+//   await waitFor(() => {
+//     const marketElement = screen.getByText("Market Price");
+//     expect(marketElement).toBeInTheDocument();
+//   });
+// });
+
+
+// it("should render the market sub-title", async () => {
+//   render(
+//     <MemoryRouter>
+//       <Market />
+//     </MemoryRouter>
+//   );
+//   const marketElement = screen.getByText("Search a currency");
+//   expect(marketElement).toBeInTheDocument();
+// });
+
+
+
+
+
+import { render, screen, fireEvent } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import Market from "../Market";
 
-it("should render the navbar title", async () => {
+
+it("should render the market title", async () => {
   render(
     <MemoryRouter>
       <Market />
     </MemoryRouter>
   );
-  const navbarElement = screen.getByTitle("header");
-  expect(navbarElement).toBeInTheDocument();
+
+  await waitFor(() => {
+    const marketElement = screen.getByText("Market Price");
+    expect(marketElement).toBeInTheDocument();
+  });
+});
+
+
+it("should render the market sub-title", async () => {
+  render(
+    <MemoryRouter>
+      <Market />
+    </MemoryRouter>
+  );
+  const marketElement = screen.getByText("Search a currency");
+  expect(marketElement).toBeInTheDocument();
+});
+
+it("should render the input field", () => {
+  render(
+    <MemoryRouter>
+      <Market />
+    </MemoryRouter>
+  );
+
+  const inputElement = screen.getByPlaceholderText("search");
+  expect(inputElement).toBeInTheDocument();
+});
+
+it("should update the input value on change", () => {
+  render(
+    <MemoryRouter>
+      <Market />
+    </MemoryRouter>
+  );
+
+  const inputElement = screen.getByPlaceholderText("search");
+  fireEvent.change(inputElement, { target: { value: "bitcoin" } });
+  expect(inputElement.value).toBe("bitcoin");
 });
