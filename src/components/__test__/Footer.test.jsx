d6177b4c91db08799e5357b04602f6d97fa76b6e
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-07-01 22:17:19 +0100

    ensured all unit tests arfe passed for the footer component

--- src/components/__test__/Footer.test.jsx
+++ src/components/__test__/Footer.test.jsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import Footer from "../Footer";
 
-it("should render the 'Sign up to learn more' text", async () => {
+it("should render the Sign up to learn more text", async () => {
   render(
     <MemoryRouter>
       <Footer />
@@ -23,6 +23,30 @@ it("should render the sign up instructions for Coinfied", async () => {
 
   const instructionText =
     "To sign up for Coinfied, visit the website, fill out the registration form, verify your email, complete any additional steps, and log in to your account.";
+
+  const footerElement = await screen.findByText(instructionText);
+  expect(footerElement).toBeInTheDocument();
+});
+
+it("should render the input field", () => {
+  render(
+    <MemoryRouter>
+      <Footer />
+    </MemoryRouter>
+  );
+
+  const inputElement = screen.getByPlaceholderText("Enter your email");
+  expect(inputElement).toBeInTheDocument();
+});
+
+it("should render the text at the bottom of the footer", async () => {
+  render(
+    <MemoryRouter>
+      <Footer />
+    </MemoryRouter>
+  );
+
+  const instructionText = "©2024 Coinfied. All rights Reserved";
 
   const footerElement = await screen.findByText(instructionText);
   expect(footerElement).toBeInTheDocument();
